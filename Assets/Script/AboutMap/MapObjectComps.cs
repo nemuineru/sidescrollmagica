6c49b7bbd07d150560003121d4fcf751ee7f4b7f
nemuineru <34166165+nemuineru@users.noreply.github.com>
2018-10-27 01:20:10 +0900

    no message

--- Assets/Script/AboutMap/MapObjectComps.cs
+++ Assets/Script/AboutMap/MapObjectComps.cs
@@ -11,16 +11,24 @@ public class MapObjectComps : MonoBehaviour {
     }
     public MoveType moveType;
 
-    public 
+    public Vector2 range;
+    public Vector2 Position;
+    [Range(0, 1f)]
+    public float progress;
+    public float Speed;
 
 	// Use this for initialization
 	void Start () {
-        
-		
+        if (moveType == MoveType.Straight)
+        {
+            StartCoroutine("MoveStraight");
+        }
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+
 }
